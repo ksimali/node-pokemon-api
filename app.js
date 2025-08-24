2f38d788b467232688e669bba5131be702d1c022
ksimali <cassim.ali@outlook.fr>
2025-08-24 11:56:01 -0400

    Implement  express framework : - installs Express and saves it as a dependency in package.json. - Create a basic nodejs web server using express library

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => res.send('Hello, Express ! '));
+app.listen(port, () => console.log(`Notre application Node est démarré sur : http://localhost:${port}`));
