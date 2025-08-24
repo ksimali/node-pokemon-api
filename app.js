b02090e71e9909048e48bc7aae7a4597a1d51275
ksimali <cassim.ali@outlook.fr>
2025-08-24 13:02:27 -0400

    Add an other endpoint to get a specific Pokémon by ID in app.js: - Create an array of pokemons with three pokemons - Add an new endpoint to get a Pokemon by id, return a response if pokemon existe, else return an error.

--- app.js
+++ app.js
@@ -3,5 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Example data
+const pokemons = [
+    { id: 1, name: 'Bulbasaur', type: 'Grass/Poison' },
+    { id: 2, name: 'Ivysaur', type: 'Grass/Poison' },
+    { id: 3, name: 'Charmander', type: 'Fire' },
+  ];
+
 app.get('/', (req,res) => res.send('Hello, Express ! '));
+
+app.get('/api/pokemons/:id', (req,res) =>{
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find(p => p.id == id);
+    
+    if(pokemon) {
+        res.json(pokemon);
+    }else{
+        res.status(404).send({ error: 'Pokemon not found' });
+    }
+});
+
 app.listen(port, () => console.log(`Notre application Node est démarré sur : http://localhost:${port}`));
